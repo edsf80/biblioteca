04f22eea845d9e8be8540a8ebfd31b20c5f1046c
edsf80 <ednaldo.dilorenzo@hotmail.com>
2022-08-15 11:20:54 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,7 +1,7 @@
 class Aluno:
 
-    def __init__(self, matricula, nome, idade, curso) -> None:
-        self.matricula = matricula
+    def __init__(self, matriculas, nome, idade, curso) -> None:
+        self.matricula = matriculas
         self.nome = nome
         self.idade = idade
         self.curso = curso
